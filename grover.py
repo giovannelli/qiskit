f1872cb48c991dd01735fcbbddff6ad93c889d2d
Duccio <giovannelli@extendi.it>
2021-05-13 00:20:02 +0200

    Create grover.py

---
+++ grover.py
@@ -0,0 +1,62 @@
+#initialization
+import matplotlib.pyplot as plt
+import numpy as np
+
+# importing Qiskit
+from qiskit import *
+from qiskit.providers.ibmq import least_busy
+
+# import basic plot tools
+from qiskit.visualization import plot_histogram
+
+# Oracle circuit
+oracle = QuantumCircuit(2,name='oracle')
+oracle.cz(0,1)
+oracle.to_gate()
+oracle.draw()
+
+# q_0: ─■─
+#       │ 
+# q_1: ─■─
+
+# Reflection circuit
+reflection = QuantumCircuit(2,name='reflection')
+reflection.h([0,1]) # Hadamard
+reflection.z([0,1])
+reflection.cz(0,1)
+reflection.h([0,1])
+reflection.to_gate()
+reflection.draw()
+
+#      ┌───┐┌───┐   ┌───┐
+# q_0: ┤ H ├┤ Z ├─■─┤ H ├
+#      ├───┤├───┤ │ ├───┤
+# q_1: ┤ H ├┤ Z ├─■─┤ H ├
+#      └───┘└───┘   └───┘
+
+grover_circuit = QuantumCircuit(2,2)
+grover_circuit.h([0,1])
+grover_circuit.append(oracle, [0,1])
+grover_circuit.append(reflection, [0,1])
+grover_circuit.measure([0,1], [0,1])
+grover_circuit.draw()
+
+#      ┌───┐┌─────────┐┌─────────────┐┌─┐   
+# q_0: ┤ H ├┤0        ├┤0            ├┤M├───
+#      ├───┤│  oracle ││  reflection │└╥┘┌─┐
+# q_1: ┤ H ├┤1        ├┤1            ├─╫─┤M├
+#      └───┘└─────────┘└─────────────┘ ║ └╥┘
+# c: 2/════════════════════════════════╩══╩═
+#                                     0  1 
+
+
+token = "cd269c86b52226ec5492ca7775cd5bb5811d0527ac73961c4b13bf3b68ea2a9571c2ba57a28bbb7098cdd1699be1549de9f508ca471e22ae88fd9572d5d05937"
+IBMQ.save_account(token)
+provider = IBMQ.load_account()
+
+device = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= 3 and 
+                                      not x.configuration().simulator and x.status().operational==True)
+
+
+job = execute(grover_circuit, backend=device, shots=1)
+plot_histogram(job.result().get_counts(), color='skyblue', title="").savefig('out.png')
